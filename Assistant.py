fd36b02a4779f81f679ec093e7aa2ec810c36844
crporter171 <60114461+crporter171@users.noreply.github.com>
2020-05-15 22:22:14 -0400

    Add files via upload

---
+++ Assistant.py
@@ -0,0 +1,53 @@
+
+import speech_recognition as sr
+from time import ctime
+import time
+import os
+from gtts import gTTS
+import requests, json
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("I am listening...")
+        audio = r.listen(source)
+    data = ""
+    try:
+        data = r.recognize_google(audio)
+        print("You said: " + data)
+    except sr.UnknownValueError:
+        print("I'm sorry, I could not understand the audio.")
+    except sr. RequestError as e:
+        print("Request Failed; {0}".format(e))
+    return data
+
+def respond(audioString):
+    print(audioString)
+    tts = gTTS(text=audioString, lang='en')
+    tts.save("speech.mp3")
+    os.system("mpg321 speech.mp3")
+
+def digital_assistant(data):
+    if "how are you" in data:
+        listening = True
+        respond("I am well, thank you for asking.")
+
+    if "what time is it" in data:
+        listening = True
+        respond(ctime())
+
+    if "stop listening" in data:
+        respond("I'll catch you later, have a blessed day.")
+        listening = False
+        print("Listening stopped")
+        return listening
+    return listening
+
+time.sleep(2)
+respond("Hi Cameron, what can I do for you?")
+listening = True
+while listening == True:
+    data = listen()
+    listening = digital_assistant(data)
+
+
